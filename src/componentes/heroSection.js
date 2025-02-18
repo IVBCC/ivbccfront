7d9955b67d5cb0b1513fdfa45d53263561116678
JesusDHernandezR <jesusdavidhernandez@unicesar.edu.co>
2025-02-18 09:23:33 -0500

    onclick de flecha

--- src/componentes/heroSection.js
+++ src/componentes/heroSection.js
@@ -6,12 +6,12 @@ const HeroSection = () => {
     <section className="hero-section" id="inicio">
       <div className="hero-content">
         <h1>Bienvenidos a Valle de Bendición</h1>
-        <img 
-          src="https://www.cruzadacristiana.org/wp-content/uploads/2024/03/Logo-Cruzada-Cristiana-Negro-1.png" 
+        <img
+          src="https://www.cruzadacristiana.org/wp-content/uploads/2024/03/Logo-Cruzada-Cristiana-Negro-1.png"
           alt="Logo Cruzada Cristiana"
           className="hero-logo"
         />
-        <div className="arrow-container">
+        <div className="arrow-container" onClick={() => window.scrollBy({ top: window.innerHeight * 0.8, behavior: 'smooth' })}>
           <span className="arrow-down"></span>
         </div>
       </div>
